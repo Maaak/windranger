f36546f27bf5fd1ede09e499346d2de2ea0c3854
Maaak <TowstonogAlexander@gmail.com>
2014-12-10 18:22:41 +0200

    Added request animation frame requestAnimationFrame polyfill by Erik Möller.
    Name refactoring

--- windranger.js
+++ windranger.js
@@ -1,5 +1,34 @@
 define(function (require, exports, module) {
 
+	// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+	// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+	// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+	// MIT license
+	function() {
+		var lastTime = 0;
+		var vendors = ['ms', 'moz', 'webkit', 'o'];
+		for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+			window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+			window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+									   || window[vendors[x]+'CancelRequestAnimationFrame'];
+		}
+	 
+		if (!window.requestAnimationFrame)
+			window.requestAnimationFrame = function(callback, element) {
+				var currTime = new Date().getTime();
+				var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+				var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+				  timeToCall);
+				lastTime = currTime + timeToCall;
+				return id;
+			};
+	 
+		if (!window.cancelAnimationFrame)
+			window.cancelAnimationFrame = function(id) {
+				clearTimeout(id);
+			};
+	}();
+
 	var AnimatedQueue = function(){
 		this.callback = null;
 		this.animationId = null;
@@ -8,39 +37,39 @@ define(function (require, exports, module) {
 		this.animate = this.animate.bind(this);
 	};
 
-	AnimatedQueue.prototype.add = function(callback, context){
-		this.items[++this.lastIndex] = callback.bind(context);
+	AnimatedQueue.prototype.push = function(callback, context){
+		this.frames[++this.pushIndex] = callback.bind(context);
 		this.start();
 	};
 
 	AnimatedQueue.prototype.animate = function(){
 		this.isAnimating = true;
-		this.callback = this.items[this.queueIndex];
+		this.callback = this.frames[this.popIndex];
 		if (this.callback) { 
 			this.callback();
-			delete this.items[this.queueIndex];
+			delete this.frames[this.popIndex];
 
-			this.queueIndex++;
-			if (this.items[this.queueIndex]) { this.animationId = window.requestAnimationFrame(this.animate); } else { this.pause(); }
+			this.popIndex++;
+			if (this.frames[this.popIndex]) { this.animationId = window.requestAnimationFrame(this.animate); } else { this.pause(); }
 		} else { 
 			this.pause();
 		}
 	};
 
 	AnimatedQueue.prototype.start = function(){
-		if (Object.keys(this.items).length > 0 && !this.isAnimating) { 
+		if (Object.keys(this.frames).length > 0 && !this.isAnimating) { 
 			this.animate(); 
 		}
 	};
 
 	AnimatedQueue.prototype.pause = function(){
-		if (!Object.keys(this.items).length) { this.reset(); }
+		if (!Object.keys(this.frames).length) { this.reset(); }
 	};
 
 	AnimatedQueue.prototype.reset = function(){
-		this.items = {};
-		this.lastIndex = -1;
-		this.queueIndex = 0;
+		this.frames = {};
+		this.pushIndex = -1;
+		this.popIndex = 0;
 		this.isAnimating = false;
 	};
 
